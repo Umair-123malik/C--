0c99700ec89b808eb333b13345bafac90e98b91c
Muhammad Umair <115361438+Umair-123malik@users.noreply.github.com>
2024-08-01 15:15:10 +0500

    Print names

---
+++ Basic programs/Printnames.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is umair haroon"<<endl;
+    cout<<"I am from lahore "<<endl;
+    cout<<"Doing bscs"<<endl;
+    cout<<"Generative ai from Piaic"<<endl;
+
+    return 0;
+}
